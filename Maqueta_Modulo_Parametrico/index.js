589b8c8fafc2ddabcae122259c542f8714c7b4b1
María Fernanda Hernández Garduño <140459150+maferbur@users.noreply.github.com>
2023-08-13 09:06:27 -0600

    Maqueta Modulo Parametrico
    Maqueta Modulo Parametrico

---
+++ Maqueta_Modulo_Parametrico/index.js
@@ -0,0 +1,47 @@
+
+var scoreInput = document.getElementById("score");
+var zonaInput = document.getElementById("zona");
+var reglaElicitaInput = document.getElementById("reglaElicita");
+var sucursalInput = document.getElementById("Sucursal");
+var divisionInput = document.getElementById("division");
+var referenciaInput = document.getElementById("referencia");
+var economiaInput = document.getElementById("economia");
+var edadInput = document.getElementById("edad");
+var ratioInput = document.getElementById("ratio");
+var claveInput = document.getElementById("clave");
+var saldosInput = document.getElementById("saldos");
+var bscInput = document.getElementById("bsc");
+var cciInput = document.getElementById("cci");
+var montoInput = document.getElementById("monto");
+var creditoInput = document.getElementById("credito");
+var productoInput = document.getElementById("producto");
+var procesoInput = document.getElementById("proceso");
+var capacidadInput = document.getElementById("capacidad");
+var bancaInput = document.getElementById("Banca");
+var evaluarButton = document.querySelector("button");
+
+
+evaluarButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    alert("Se ha hecho clic en el botón");
+    var scoreValue = scoreInput.value;
+    var zonaValue = zonaInput.value;
+    var reglaElicitaValue = reglaElicitaInput.value;
+    var sucursalValue = sucursalInput.value;
+    var divisionValue = divisionInput.value;
+    var referenciaValue = referenciaInput.value;
+    var economiaValue = economiaInput.value;
+    var edadValue = edadInput.value;
+    var ratioValue = ratioInput.value;
+    var claveValue = claveInput.value;
+    var saldosValue = saldosInput.value;
+    var bscValue = bscInput.value;
+    var cciValue = cciInput.value;
+    var montoValue = montoInput.value;
+    var creditoValue = creditoInput.value;
+    var productoValue = productoInput.value;
+    var procesoValue = procesoInput.value;
+    var capacidadValue = capacidadInput.value;
+    var bancaValue = bancaInput.value;
+
+});
